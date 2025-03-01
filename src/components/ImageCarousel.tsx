3482d2b500b84b37e1f030dee18ac61858bd6a90
DaKheera47 <shaheergoodboy@gmail.com>
2025-03-01 12:08:09 +0000

    hero

---
+++ src/components/ImageCarousel.tsx
@@ -0,0 +1,27 @@
+import { type FC, useState, useEffect } from 'react';
+
+interface ImageCarouselProps {
+  images: string[];
+  className?: string;
+}
+
+export const ImageCarousel: FC<ImageCarouselProps> = ({
+  images,
+  className,
+}) => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex(currentIndex => (currentIndex + 1) % images.length);
+    }, 2500);
+
+    return () => clearInterval(interval);
+  }, [images]);
+
+  return (
+    <div className={className}>
+      <img src={images[index]} alt={`Carousel image ${index + 1}`} />
+    </div>
+  );
+};
